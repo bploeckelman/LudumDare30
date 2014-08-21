363cf73436b4a03dbadffce319c88eae01b6ab39
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2014-08-20 22:51:54 -0500

    LibGDX 1.3.1 Skeleton

---
+++ android/src/lando/systems/ld30/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package lando.systems.ld30.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import lando.systems.ld30.LudumDare30;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new LudumDare30(), config);
+	}
+}
---
+++ html/src/lando/systems/ld30/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package lando.systems.ld30.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import lando.systems.ld30.LudumDare30;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new LudumDare30();
+        }
+}
