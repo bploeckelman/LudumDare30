fa10652e5b4ab26faaf70a6593308db3e7efaa8a
Doug Graham <dsgraham@gmail.com>
2014-08-25 21:28:08 -0500

    Fix for deploy

--- desktop/src/lando/systems/ld30/desktop/DesktopLauncher.java
+++ desktop/src/lando/systems/ld30/desktop/DesktopLauncher.java
@@ -13,27 +13,27 @@ public class DesktopLauncher {
     final static boolean PARSE_SVG = false;
 
     public static void main (String[] arg) {
-
-        if (PARSE_SVG) {
-
-            SVGConverter svgc = new SVGConverter();
-            try {
-                svgc.run("../../art/svg-to-parse");
-            } catch (Exception e) {
-                System.out.println("failed to parse svgs");
-            }
-
-        }
-        
-        TexturePacker.Settings settings = new TexturePacker.Settings();
-        settings.filterMin = Texture.TextureFilter.MipMapNearestNearest;
-        settings.filterMag = Texture.TextureFilter.MipMapNearestNearest;
-//        settings.filterMin = Texture.TextureFilter.Linear;
-//        settings.filterMag = Texture.TextureFilter.Linear;
-        settings.maxWidth = 1024;
-        settings.maxHeight = 1024;
-        TexturePacker.process(settings, "./images", "./atlas", "game");
-//        TexturePacker.process(settings, "./prism", "./atlas", "prism");
+//
+//        if (PARSE_SVG) {
+//
+//            SVGConverter svgc = new SVGConverter();
+//            try {
+//                svgc.run("../../art/svg-to-parse");
+//            } catch (Exception e) {
+//                System.out.println("failed to parse svgs");
+//            }
+//
+//        }
+//
+//        TexturePacker.Settings settings = new TexturePacker.Settings();
+//        settings.filterMin = Texture.TextureFilter.MipMapNearestNearest;
+//        settings.filterMag = Texture.TextureFilter.MipMapNearestNearest;
+////        settings.filterMin = Texture.TextureFilter.Linear;
+////        settings.filterMag = Texture.TextureFilter.Linear;
+//        settings.maxWidth = 1024;
+//        settings.maxHeight = 1024;
+//        TexturePacker.process(settings, "./images", "./atlas", "game");
+////        TexturePacker.process(settings, "./prism", "./atlas", "prism");
 
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
         config.width = Config.window_width;
